cb6d4c8ac3e188893845de5935a7cb216d3d80e3
DevoEzk <devoezekiel30@gmail.com>
2024-08-08 20:20:59 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+password = ""
+
+for i in range(12):
+    password = password + random.choice(characters)
+
+print(password)
+
+
+
+
+
